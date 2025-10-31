db5588f0ec3734e561369f01237fdfe716106dfb
alonzogravel92782-png <alonzogravel92782@gmail.com>
2025-10-31 10:23:30 +0200

    I finished exercise 4 using Javascript

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+// TODO: Create a variable called score and give it a value between 0 and 100 
+let score = 88
+// TODO: Create a variable called passingGrade and set it to 60 
+const passingGrade = 60
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade 
+if (score >= passingGrade){
+}
+// TODO: Add an else statement for when the score is below passingGrade 
+if (score < passingGrade){
+
+}
+// TODO: Print "Passed" or "Failed" based on the score 
+console.log("Congratulations, you have passed")
+console.log('Unfrotunately, you have failed')
+// TODO: Add another condition to check if the score is perfect (100)
+if (score == 100){
+    console.log("Perfect!!")
+}
